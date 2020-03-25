ef58eb9bbe0121ecedc0c3d91988315d00e4aea1
alexpejovic <62117049+alexpejovic@users.noreply.github.com>
2020-03-25 16:14:00 -0400

    added files to .gitignore and fixed player.generate_goals

--- player.py
+++ player.py
@@ -287,7 +287,7 @@ class RandomPlayer(Player):
         if not self._proceed:
             return None
 
-        actions = KEY_ACTION.values()
+        actions = list(KEY_ACTION.values())
         actions.remove(PASS)
         has_valid = False
         board = board.create_copy()
